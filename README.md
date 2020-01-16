e047d1c198f63202e72b683c4ef2011b966f1eec
Ryan Sullivan <rpsulli@gmail.com>
2020-01-15 22:04:31 -0800

    simple server

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # bounty-server
 Simple node.js express server used bug bounty POCs.
+
+**Requirements**
+Must install node.js v6+ (supporting ES6)
+
+**Install**
+```
+npm install
+```
+
+**Start Server:**
+```
+node index.js
+```
+
+**Display files in /captured directory:**
+```
+http://localhost:9000/
+```
+
+**Example request that writes query string to file:**
+```
+http://localhost:9000/c/?c=my-captured-string
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const ecstatic = require('ecstatic');
+const http = require('http');
+const fs = require('fs');
+const moment = require('moment');
+
+const app = express();
+
+app.use(ecstatic({
+  root: `${__dirname}/captured`,
+  showdir: true,
+}));
+
+app.get('/c/', (req, res) => {
+  const c = req.query.c;
+  console.log(`Incoming: ${c}`);
+
+  const date = moment().format();
+
+  fs.writeFile(`captured/captured_${date}.txt`, c, (err) => {
+    console.log(`Created: captured_${date}.txt`);
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Captured!');
+  });
+  res.send('Success!');
+});
+
+http.createServer(app).listen(9000);
+
+console.log('Listening on :9000');
